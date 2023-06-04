2c1ecf12dd91e2a06fd6591612d6cda919d81ddd
ElectronWrangler <128319092+ElectronWranglerII@users.noreply.github.com>
2023-06-04 01:05:33 -0700

    Update table.js

--- HTML Constructors/table.js
+++ HTML Constructors/table.js
@@ -1,8 +1,10 @@
-;* HTML Table Utilities
-;* Copyright (C) DeRemee Systems, IXE Electronics LLC
-;* Portions copyright IXE Electronics LLC, Republic Robotics, FemtoLaunch, FemtoSat, FemtoTrack, Weland
-;* This work is made available under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
-;* To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+/*
+* HTML Table Utilities
+* Copyright (C) DeRemee Systems, IXE Electronics LLC
+* Portions copyright IXE Electronics LLC, Republic Robotics, FemtoLaunch, FemtoSat, FemtoTrack, Weland
+* This work is made available under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+* To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+*/
 
 class HTMLTable{
 	constructor(){
